a45a42a1666c182b9d471b796dfdec6fda932264
zhongyang <1224623904@qq.com>
2018-07-02 19:32:06 +0800

    Initial Commit

---
+++ GitPractice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitPractice
+//
+//  Created by 钟杨 on 2018/7/2.
+//  Copyright © 2018年 钟杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
